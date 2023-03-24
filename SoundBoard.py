ff44e1c113db0d8b5dc9d818b5811898556ad269
unknown <kjl561@gmail.com>
2023-03-24 17:06:19 -0400

    playback options

--- SoundBoard.py
+++ SoundBoard.py
@@ -26,10 +26,16 @@ class SoundBoard:
         self.button_grid = tk.Frame(master)
         self.button_grid.grid(row=1, column=0, columnspan=3)
 
-    def load_sound(self):
-        # Get the filepath from the entry
-        filepath = self.filepath_entry.get()
+        # Create playback buttons
+        self.rewind_button = tk.Button(master, text="Rewind", command=self.rewind_sound)
+        self.rewind_button.grid(row=2, column=0)
+        self.slowdown_button = tk.Button(master, text="Slow Down", command=self.slowdown_sound)
+        self.slowdown_button.grid(row=2, column=1)
 
+    def load_sound(self):
+        # Get the filepath from the file explorer
+        filepath = tk.filedialog.askopenfilename(initialdir="/", title="Select MP3 file", filetypes=(("MP3 files", "*.mp3"), ("all files", "*.*")))
+        
         # Load the sound file with Pygame
         sound = pygame.mixer.Sound(filepath)
 
@@ -47,6 +53,41 @@ class SoundBoard:
 
         # Play the sound
         sound.play()
+
+    def rewind_sound(self):
+        # Stop any currently playing sounds
+        pygame.mixer.stop()
+
+        # Get the current playback position
+        current_pos = pygame.mixer.music.get_pos()
+
+        # Rewind the sound by 5 seconds (5000 milliseconds)
+        new_pos = current_pos - 5000
+        if new_pos < 0:
+            new_pos = 0
+
+        # Set the new playback position
+        pygame.mixer.music.set_pos(new_pos)
+
+        # Resume playback
+        pygame.mixer.music.unpause()
+
+    def slowdown_sound(self):
+        # Stop any currently playing sounds
+        pygame.mixer.stop()
+
+        # Get the current playback rate
+        current_rate = pygame.mixer.music.get_rate()
+
+        # Slow down the playback rate by 50%
+        new_rate = int(current_rate * 0.5)
+
+        # Set the new playback rate
+        pygame.mixer.music.set_rate(new_rate)
+
+        # Resume playback
+        pygame.mixer.music.unpause()
+
 
 # Create the root window
 root = tk.Tk()
